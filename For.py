83415627edf5d49c240d636024c020e2cfa1709b
Arman11980 <165942302+Arman11980@users.noreply.github.com>
2024-04-19 21:48:39 +0300

    Update For.py

--- For.py
+++ For.py
@@ -1,9 +1,6 @@
 list_ = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
-for i in list_:
-    print('я езжу на автомобиле ' + i)
 cars_count_ = 0
-for j in range(0, 100, 10):
-     print(j)
-     # 3.Создайте целочисленную переменную cars_count = 0
-     # 4.Напишите в цикле из шага номер 2 увеличение переменной на 10 в каждом шаге цикла (cars_count += 10)
-     #  я не понял вопрос
+for i in list_:
+    cars_count_ += 10
+    print('я езжу на автомобиле ', + i, cars_count_)
+     
